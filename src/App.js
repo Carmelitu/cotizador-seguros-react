afa63fc351f79ec6f84216e8c744a865f12cac6e
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-07 23:28:14 -0300

    se agrega formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+const ContenedorForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`
+
 
 
 function App() {
   return (
-    <Fragment>
+    <Contenedor>
       <Header titulo='Cotizador de Seguros' />
-    </Fragment>
+
+      <ContenedorForm>
+        <Formulario />
+      </ContenedorForm>
+
+    </Contenedor>
   );
 }
 
